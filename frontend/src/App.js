a548851ea50cd290a1fb0f046f9d1990d802c393
roromer0 <roromero147@gmail.com>
2023-06-26 19:13:50 +0200

    Some more componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
+import React from "react";
 import { useEffect } from "react";
 import useFetch from "./hooks/useFetch";
-import HourGlass from "./components/UI/Spinner/HourGlass";
-import TasksList from "./components/Tasks/TasksList";
-import TasksForm from "./components/Tasks/TasksForm";
 import classes from "./App.module.css";
+import Login from "./components/Login/Login";
 
 const App = () => {
   const { pending, error, tasks, fetchTasksHandler } = useFetch();
@@ -29,35 +28,7 @@ const App = () => {
 
   return (
     <main>
-      <section className={classes["task-form"]}>
-        <TasksForm onAddTask={addTaskHandler} />
-      </section>
-      {
-        // mientras se carga la lista de tareas, se muestra el spinner
-        pending && <HourGlass />
-      }
-      <section className={classes["tasks-content"]}>
-        {
-          // si hay tareas, y no hay errores, y no está cargando, se muestran las tareas
-          !pending && !error && tasks.length > 0 && (
-            <TasksList
-              items={tasks}
-              onDeleteItem={deleteItemHandler}
-              onUpdateItem={updateItemHandler}
-            />
-          )
-        }
-        {
-          // si no hay tareas, y no hay errores, y no está cargando, se muestra el mensaje de que no hay tareas
-          !pending && !error && tasks.length === 0 && (
-            <p className={classes["no-tasks"]}>No pending tasks!</p>
-          )
-        }
-        {
-          // si hay errores, se muestra el mensaje de error
-          error && <p className={classes["error"]}>{error.message}</p>
-        }
-      </section>
+      <Login />
     </main>
   );
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Login from "./components/Login/Login";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>
 );
